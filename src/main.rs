862aa5643e2f6ee1ae9f4622b9c9485c112a876e
Ganesha Danu <ganeshadanu1@gmail.com>
2019-03-25 00:49:40 +0700

    implement basic json response

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,36 @@
 extern crate gotham;
 extern crate hyper;
 extern crate mime;
+#[macro_use]
+extern crate serde_derive;
+extern crate serde_json;
+
+use hyper::{ Body, Response, StatusCode };
 
 use gotham::router::Router;
 use gotham::router::builder::*;
 use gotham::state::State;
+use gotham::handler::IntoResponse;
+use gotham::helpers::http::response::create_response;
 
+#[derive(Serialize)]
+pub struct User {
+    username: String
+}
 
-pub fn index(state: State) -> (State, &'static str) {
-    (state, "Hello World")
+impl IntoResponse for User {
+    fn into_response(self, state: &State) -> Response<Body> {
+        create_response(
+            state,
+            StatusCode::OK,
+            mime::APPLICATION_JSON,
+            serde_json::to_string(&self).expect("serialized user")
+        )
+    }
+}
+
+pub fn index(state: State) -> (State, User) {
+    (state, User { username: "blinfoldking".to_string() })
 }
 
 fn router() -> Router {
@@ -24,3 +46,4 @@ fn main() {
     gotham::start(addr, router());
     println!("Hello, world!");
 }
+
